ff03fda567de2f627c6fe30ce973c7455413b41e
DoctorJohn <github@ehwald.info>
2018-01-29 02:08:09 +0100

    Extends the HelperAPIs functionality

--- botfather/scripting/helper_api.cpp
+++ botfather/scripting/helper_api.cpp
@@ -1,5 +1,7 @@
 #include "helper_api.h"
 #include <QFileInfo>
+#include <QThread>
+#include "../shared/constants.h"
 
 HelperAPI::HelperAPI(QJSEngine* engine_p) : m_engine_p(engine_p)
 {}
@@ -15,4 +17,44 @@ bool HelperAPI::fileExists(QString file_path)
 {
 	QFileInfo file(file_path);
 	return file.exists() && file.isFile();
+}
+
+void HelperAPI::sleep(int seconds)
+{
+	QThread::sleep(seconds);
+}
+
+void HelperAPI::msleep(int milliseconds)
+{
+	QThread::msleep(milliseconds);
+}
+
+int HelperAPI::getVersionMajor()
+{
+	return constants::VERSION_MAJOR;
+}
+
+int HelperAPI::getVersionMinor()
+{
+	return constants::VERSION_MINOR;
+}
+
+int HelperAPI::getVersionPatch()
+{
+	return constants::VERSION_PATCH;
+}
+
+QString HelperAPI::getClientType()
+{
+	return constants::CLIENT_TYPE;
+}
+
+QString HelperAPI::getClientMode()
+{
+	return "gui"; // TODO: Make this actually based on the clients mode.
+}
+
+void HelperAPI::log(QString log_message)
+{
+	// TODO: Implement this
 }
--- botfather/scripting/helper_api.h
+++ botfather/scripting/helper_api.h
@@ -12,7 +12,33 @@ class HelperAPI : public QObject
 public:
 	HelperAPI(QJSEngine* engine_p);
 	static void enable(QJSEngine* engine_p);
+	
+	// Returns true if the file exists.
 	Q_INVOKABLE static bool fileExists(QString file_path);
+	
+	// Makes the bot sleep for x seconds.
+	Q_INVOKABLE static void sleep(int seconds);
+	
+	// Makes the bot sleep for x milliseconds.
+	Q_INVOKABLE static void msleep(int milliseconds);
+	
+	// Returns the client major version number.
+	Q_INVOKABLE static int getVersionMajor();
+	
+	// Returns the client minor version number.
+	Q_INVOKABLE static int getVersionMinor();
+	
+	// Returns the client path version number.
+	Q_INVOKABLE static int getVersionPatch();
+	
+	// Returns the botfathers client type (browser, desktop, android).
+	Q_INVOKABLE static QString getClientType();
+	
+	// Returns the programs mode (gui, console).
+	Q_INVOKABLE static QString getClientMode();
+	
+	// Logs the message in the users chat.
+	Q_INVOKABLE void log(QString log_message);
 	
 private:
 	QJSEngine* m_engine_p;
