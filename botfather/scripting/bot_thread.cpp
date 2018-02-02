3f902bbd6301aad1d98170840f84809284b65e5a
DoctorJohn <github@ehwald.info>
2018-02-02 22:34:26 +0100

    Makes the protected function QThread::setTerminationEnabled accessible

---
+++ botfather/scripting/bot_thread.cpp
@@ -0,0 +1,6 @@
+#include "bot_thread.h"
+
+void BotThread::setTerminationEnabled(bool enabled)
+{
+	this->QThread::setTerminationEnabled(enabled);
+}
--- botfather/scripting/bot_thread.h
+++ botfather/scripting/bot_thread.h
@@ -1,4 +1,12 @@
-#ifndef BOT_THREAD_H
-#define BOT_THREAD_H
+#ifndef BFB__SCRIPTING__BOT_THREAD_H
+#define BFB__SCRIPTING__BOT_THREAD_H
 
-#endif // BOT_THREAD_H
+#include <QThread>
+
+class BotThread : public QThread
+{
+public:
+	void setTerminationEnabled(bool enabled=true);
+};
+
+#endif // BFB__SCRIPTING__BOT_THREAD_H
