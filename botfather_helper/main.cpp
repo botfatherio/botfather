5cece6f82e643bcc5e47ddeffd756f54fc3ace91
DoctorJohn <github@ehwald.info>
2018-02-07 18:51:15 +0100

    Adds correct win 64bit entrypoint and removes win 32bit entrypoint support

--- botfather_helper/main.cpp
+++ botfather_helper/main.cpp
@@ -2,28 +2,16 @@
 #include "include/cef_app.h"
 #include "include/cef_client.h"
 
-#if (defined (_WIN32) || defined (_WIN64))
-#include <Windows.h>
-#endif
+#ifdef _WIN64
+#include <windows.h>
 
-#if defined (_WIN32)
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow)
 {
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
-	CefMainArgs main_args(hInstance);
-	return CefExecuteProcess(main_args, CefRefPtr<CefApp>(), nullptr);
-}
-
-#elif defined (_WIN64)
-
-int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
-{
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
-	CefMainArgs main_args(hInstance);
-	return CefExecuteProcess(main_args, CefRefPtr<CefApp>(), nullptr);
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(nCmdShow);
+    CefMainArgs main_args(hInstance);
+    return CefExecuteProcess(main_args, CefRefPtr<CefApp>(), nullptr);
 }
 
 #else
