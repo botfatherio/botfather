342b64c5d3cf723ac181b0719e09e9059cfc8891
DoctorJohn <github@ehwald.info>
2018-02-10 01:27:34 +0100

    Makes Match no longer derive from QRect and make it's x,y values reference the center of the match

--- botfather/vision/match.cpp
+++ botfather/vision/match.cpp
@@ -1,11 +1,24 @@
 #include "match.h"
 
-Match::Match() : QRect()
+Match::Match()
 {}
 
-Match::Match(double score, QRect rect) : QRect(rect)
+Match::Match(double score, int left, int top, int width, int height)
 {
 	this->m_score = score;
+	this->m_width = width;
+	this->m_height = height;
+	this->m_left = left;
+	this->m_right = left + width;
+	this->m_top = top;
+	this->m_bottom = top + height;
+	this->m_x = left + (width / 2);
+	this->m_y = top + (height / 2);
+}
+
+bool Match::found() const
+{
+	return m_score != -1;
 }
 
 double Match::getScore() const
@@ -15,40 +28,40 @@ double Match::getScore() const
 
 int Match::getLeft() const
 {
-	return this->QRect::left();
+	return this->m_left;
 }
 
 int Match::getRight() const
 {
-	return this->QRect::right();
+	return this->m_right;
 }
 
 int Match::getTop() const
 {
-	return this->QRect::top();
+	return this->m_top;
 }
 
 int Match::getBottom() const
 {
-	return this->QRect::bottom();
+	return this->m_bottom;
 }
 
 int Match::getWidth() const
 {
-	return this->QRect::width();
+	return this->m_width;
 }
 
 int Match::getHeight() const
 {
-	return this->QRect::height();
+	return this->m_height;
 }
 
 int Match::getX() const
 {
-	return this->QRect::x();
+	return this->m_x;
 }
 
 int Match::getY() const
 {
-	return this->QRect::y();
+	return this->m_y;
 }
--- botfather/vision/match.h
+++ botfather/vision/match.h
@@ -4,14 +4,15 @@
 #include <QObject>
 #include <QRect>
 
-class Match : public QObject, public QRect
+class Match : public QObject
 {
 	Q_OBJECT
 	
 public:
 	Match();
-	Match(double score, QRect rect);
+	Match(double score, int left, int top, int width, int height);
 
+	Q_INVOKABLE bool found() const;
 	Q_INVOKABLE double getScore() const;
 	Q_INVOKABLE int getLeft() const;
 	Q_INVOKABLE int getRight() const;
@@ -24,7 +25,16 @@ public:
 	
 private:
 	// The tpls matching score on the reference (0.0 to 1.0).
-	double m_score = 0.0;
+	double m_score = -1;
+	
+	int m_left = 0;
+	int m_top = 0;
+	int m_width = 0;
+	int m_height = 0;
+	int m_right = 0;
+	int m_bottom = 0;
+	int m_x = 0;
+	int m_y = 0;
 };
 
 #endif // BFP__VISION__MATCH_H
