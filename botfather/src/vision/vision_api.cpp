e985bf1ac2910bd6748f59f1d6a48ca3e4b114ec
DoctorJohn <github@ehwald.info>
2018-05-12 03:46:47 +0200

    Add user param check to VisionAPI methods

--- botfather/src/vision/vision_api.cpp
+++ botfather/src/vision/vision_api.cpp
@@ -24,6 +24,14 @@ void VisionAPI::enable(Bot* bot_p, QScriptEngine* engine_p)
 
 void VisionAPI::saveImage(Image* image, QString path)
 {
+	if (!image) {
+		m_engine_p->currentContext()->throwError("Invalid image.");
+		return;
+	}
+	if (path.isEmpty()) {
+		m_engine_p->currentContext()->throwError("Empty path.");
+		return;
+	}
 	path = this->m_bot_p->normalisePath(path);
 	Vision::saveImage(image->getUMat(), path);
 }
@@ -33,12 +41,20 @@ QScriptValue VisionAPI::loadImage(QString path) {
 	if (this->m_bot_p->fileExists(path)){
 		return m_engine_p->newQObject(new Image(Vision::loadImage(path)));
 	}
-	// Trow exception instead?
-	return m_engine_p->newQObject(new Image());
+	return m_engine_p->currentContext()->throwError("Invalid path.");
 }
 
 QScriptValue VisionAPI::cropImage(Image* image, int x_offset, int y_offset, int width, int height)
 {
+	if (!image) {
+		return m_engine_p->currentContext()->throwError("Invalid image.");
+	}
+	if (x_offset < 0 || y_offset < 0) {
+		return m_engine_p->currentContext()->throwError("Offset must not be negativ.");
+	}
+	if (width <= 0 || height <= 0) {
+		return m_engine_p->currentContext()->throwError("Width and height must be at least 1.");
+	}
 	QRect region(x_offset, y_offset, width, height);
 	cv::UMat cropped_image = Vision::cropImage(image->getUMat(), region);
 	return m_engine_p->newQObject(new Image(cropped_image));
@@ -46,29 +62,58 @@ QScriptValue VisionAPI::cropImage(Image* image, int x_offset, int y_offset, int 
 
 QScriptValue VisionAPI::grayImage(Image *image)
 {
+	if (!image) {
+		return m_engine_p->currentContext()->throwError("Invalid image.");
+	}
 	cv::UMat gray_image = Vision::grayImage(image->getUMat());
 	return m_engine_p->newQObject(new Image(gray_image));
 }
 
 QScriptValue VisionAPI::resizeImage(Image *image, int new_width, int new_height)
 {
+	if (!image) {
+		return m_engine_p->currentContext()->throwError("Invalid image.");
+	}
+	if (new_width <= 0 || new_height <= 0) {
+		return m_engine_p->currentContext()->throwError("Width and height must be at least 1.");
+	}
 	cv::UMat resized_image = Vision::resizeImage(image->getUMat(), new_width, new_height);
 	return m_engine_p->newQObject(new Image(resized_image));
 }
 
 QScriptValue VisionAPI::isolateColor(Image *image, HSVColor* min_hsv, HSVColor* max_hsv, bool keep_color)
 {
+	if (!image) {
+		return m_engine_p->currentContext()->throwError("Invalid image.");
+	}
+	if (!min_hsv || !max_hsv) {
+		m_engine_p->currentContext()->throwError("Invalid HSV color.");
+	}
 	cv::UMat result_image = Vision::isolateColor(image->getUMat(), min_hsv->getScalar(), max_hsv->getScalar(), keep_color);
 	return m_engine_p->newQObject(new Image(result_image));
 }
 
 bool VisionAPI::sameImages(Image* image_1, Image* image_2)
 {
+	if (!image_1 || !image_2) {
+		m_engine_p->currentContext()->throwError("Invalid image.");
+		return false;
+	}
 	return Vision::sameImages(image_1->getUMat(), image_2->getUMat());
 }
 
 QScriptValue VisionAPI::findMaskedMatches(Image *image, Image *tpl, Image *mask, double threshold, int max_matches)
 {
+	if (!image) {
+		return m_engine_p->currentContext()->throwError("Invalid image.");
+	}
+	if (!tpl) {
+		return m_engine_p->currentContext()->throwError("Invalid template.");
+	}
+	if (!mask) {
+		return m_engine_p->currentContext()->throwError("Invalid mask.");
+	}
+	
 	QVector<Match*> matches = Vision::findMaskedMatches(image->getUMat(), tpl->getUMat(), mask->getUMat(), threshold, max_matches);
 	QScriptValue js_matches = m_engine_p->newArray();
 	
@@ -82,6 +127,15 @@ QScriptValue VisionAPI::findMaskedMatches(Image *image, Image *tpl, Image *mask,
 
 QScriptValue VisionAPI::findMaskedMatch(Image *image, Image *tpl, Image *mask, double threshold)
 {
+	if (!image) {
+		return m_engine_p->currentContext()->throwError("Invalid image.");
+	}
+	if (!tpl) {
+		return m_engine_p->currentContext()->throwError("Invalid template.");
+	}
+	if (!mask) {
+		return m_engine_p->currentContext()->throwError("Invalid mask.");
+	}
 	Match* match = Vision::findMaskedMatch(image->getUMat(), tpl->getUMat(), mask->getUMat(), threshold);
 	return m_engine_p->newQObject(match);
 }
@@ -89,16 +143,34 @@ QScriptValue VisionAPI::findMaskedMatch(Image *image, Image *tpl, Image *mask, d
 
 QScriptValue VisionAPI::findMatches(Image *image, Image *tpl, double threshold, int max_matches)
 {
+	if (!image) {
+		return m_engine_p->currentContext()->throwError("Invalid image.");
+	}
+	if (!tpl) {
+		return m_engine_p->currentContext()->throwError("Invalid template.");
+	}
 	return VisionAPI::findMaskedMatches(image, tpl, new Image(), threshold, max_matches);
 }
 
 QScriptValue VisionAPI::findMatch(Image *image, Image *tpl, double threshold)
 {
+	if (!image) {
+		return m_engine_p->currentContext()->throwError("Invalid image.");
+	}
+	if (!tpl) {
+		return m_engine_p->currentContext()->throwError("Invalid template.");
+	}
 	return VisionAPI::findMaskedMatch(image, tpl, new Image(), threshold);
 }
 
 QScriptValue VisionAPI::findBlobs(BlobTpl *blob_tpl, Image *image)
 {
+	if (!image) {
+		return m_engine_p->currentContext()->throwError("Invalid image.");
+	}
+	if (!blob_tpl) {
+		return m_engine_p->currentContext()->throwError("Invalid template.");
+	}
 	QVector<cv::KeyPoint> keypoints = Vision::findBlobs(blob_tpl, image->getUMat());
 	
 	QScriptValue matches = m_engine_p->newArray();
@@ -120,8 +192,11 @@ QScriptValue VisionAPI::findBlobs(BlobTpl *blob_tpl, Image *image)
 
 QScriptValue VisionAPI::markMatches(Image *image, QScriptValue matches, int r, int g, int b, int thickness)
 {
+	if (!image) {
+		return m_engine_p->currentContext()->throwError("Invalid image.");
+	}
 	if (!matches.isArray()) {
-		return QScriptValue();
+		return m_engine_p->currentContext()->throwError("Matches must be an array.");
 	}
 	
 	int length = matches.property("length").toNumber();
@@ -146,6 +221,12 @@ QScriptValue VisionAPI::markMatches(Image *image, QScriptValue matches, int r, i
 
 QScriptValue VisionAPI::markMatch(Image *image, Match *match, int r, int g, int b, int thickness)
 {
+	if (!image) {
+		return m_engine_p->currentContext()->throwError("Invalid image.");
+	}
+	if (!match) {
+		return m_engine_p->currentContext()->throwError("Invalid match.");
+	}
 	cv::UMat result_image = Vision::markMatch(image->getUMat(), match, cv::Scalar(b, g, r), thickness);
 	return m_engine_p->newQObject(new Image(result_image));
 }
