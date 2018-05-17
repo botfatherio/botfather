eb1edfc0a4a6c0b7964bd3ba18c8fbbcdcd265c1
DoctorJohn <github@ehwald.info>
2018-05-17 03:52:23 +0200

    Rewrite the blob tpl factory with QScriptEngine

--- botfather/src/vision/blob_tpl_factory.cpp
+++ botfather/src/vision/blob_tpl_factory.cpp
@@ -1,98 +1,53 @@
 #include "blob_tpl_factory.h"
 #include "blob_tpl.h"
+#include <QDebug>
 
 BlobTplFactory::BlobTplFactory(QScriptEngine *engine_p)
 	: m_engine_p(engine_p)
 {}
 
-QScriptValue BlobTplFactory::createInstance(
-	HSVColor* min_hsv,
-	HSVColor* max_hsv,
-	bool filter_by_area,
-	int min_area,
-	int max_area,
-	bool filter_by_circularity,
-	double min_circularity,
-	double max_circularity,
-	bool filter_by_convexity,
-	double min_convexity,
-	double max_convexity,
-	bool filter_by_inertia,
-	double min_inertia_ration,
-	double max_inertia_ration)
-{
-	// The engine takes ownership and destroys the object if no longer required.
-	return m_engine_p->newQObject(new BlobTpl(
-		min_hsv,
-		max_hsv,
-		filter_by_area,
-		min_area,
-		max_area,
-		filter_by_circularity,
-		min_circularity,
-		max_circularity,
-		filter_by_convexity,
-		min_convexity,
-		max_convexity,
-		filter_by_inertia,
-		min_inertia_ration,
-		max_inertia_ration 
-	));
-}
-
 // static
 void BlobTplFactory::enable(QScriptEngine *engine_p)
 {
-	QScriptValue hsv_color_factory = engine_p->newQObject(new BlobTplFactory(engine_p));
-	engine_p->globalObject().setProperty("_blob_tpl_factory", hsv_color_factory);
-	engine_p->evaluate(
-		"function BlobTpl("
-			"min_hsv,"
-			"max_hsv,"
-			"filter_by_area,"
-			"min_area,"
-			"max_area,"
-			"filter_by_circularity,"
-			"min_circularity,"
-			"max_circularity,"
-			"filter_by_convexity,"
-			"min_convexity,"
-			"max_convexity,"
-			"filter_by_inertia,"
-			"min_inertia_ration,"
-			"max_inertia_ration)"
-		"{"
-			//"if (typeof(min_hsv)==='undefined') min_hsv"
-			//"if (typeof(max_hsv)==='undefined') max_hsv"
-			//"if (typeof(filter_by_area)==='undefined') filter_by_area"
-			"if (typeof(min_area)==='undefined') min_area = 0.0;"
-			"if (typeof(max_area)==='undefined') max_area = 0.0;"
-			"if (typeof(filter_by_circularity)==='undefined') filter_by_circularity = false;"
-			"if (typeof(min_circularity)==='undefined') min_circularity = 0.0;"
-			"if (typeof(max_circularity)==='undefined') max_circularity = 0.0;"
-			"if (typeof(filter_by_convexity)==='undefined') filter_by_convexity = false;"
-			"if (typeof(min_convexity)==='undefined') min_convexity = 0.0;"
-			"if (typeof(max_convexity)==='undefined') max_convexity = 0.0;"
-			"if (typeof(filter_by_inertia)==='undefined') filter_by_inertia = false;"
-			"if (typeof(min_inertia_ration)==='undefined') min_inertia_ration = 0.0;"
-			"if (typeof(max_inertia_ration)==='undefined') max_inertia_ration = 0.0;"
-			""
-			"return _blob_tpl_factory.createInstance("
-				"min_hsv,"
-				"max_hsv,"
-				"filter_by_area,"
-				"min_area,"
-				"max_area,"
-				"filter_by_circularity,"
-				"min_circularity,"
-				"max_circularity,"
-				"filter_by_convexity,"
-				"min_convexity,"
-				"max_convexity,"
-				"filter_by_inertia,"
-				"min_inertia_ration,"
-				"max_inertia_ration"
-			");"
-		"}"
-	);
+	engine_p->globalObject().setProperty("BlobTpl", engine_p->newFunction(BlobTplFactory::create));
+}
+
+QScriptValue BlobTplFactory::create(QScriptContext *context, QScriptEngine *engine)
+{
+	if (context->argumentCount() < 2) {
+		return context->throwError("Too few arguments.");
+	}
+	if (!context->argument(0).isQObject() || (qobject_cast<HSVColor*>(context->argument(0).toQObject()) == 0)) {
+		return context->throwError("Argument 'hsv_min' must be an HSVColor object.");
+	}
+	if (!context->argument(1).isQObject() || (qobject_cast<HSVColor*>(context->argument(1).toQObject()) == 0)) {
+		return context->throwError("Argument 'max_hsv' must be an HSVColor object.");
+	}
+	
+	HSVColor *min_hsv = qobject_cast<HSVColor*>(context->argument(0).toQObject());
+	HSVColor *max_hsv = qobject_cast<HSVColor*>(context->argument(1).toQObject());
+	
+	int min_area = context->argument(3).isNumber() ? context->argument(3).toNumber() : -1;
+	int max_area = context->argument(4).isNumber() ? context->argument(4).toNumber() : -1;
+	bool filter_by_area = min_area > 0 && max_area > 0 && context->argument(2).isBool() && context->argument(2).toBool();
+	
+	double min_circularity = context->argument(6).isNumber() ? context->argument(6).toNumber() : -1;
+	double max_circularity = context->argument(7).isNumber() ? context->argument(7).toNumber() : -1;
+	bool filter_by_circularity = min_circularity > 0 && max_circularity > 0 && context->argument(5).isBool() && context->argument(5).toBool();
+	
+	double min_convexity = context->argument(9).isNumber() ? context->argument(9).toNumber() : -1;
+	double max_convexity = context->argument(10).isNumber() ? context->argument(10).toNumber() : -1;
+	bool filter_by_convexity = min_convexity > 0 && max_convexity > 0 && context->argument(8).isBool() && context->argument(8).toBool();
+	
+	double min_inertia = context->argument(12).isNumber() ? context->argument(12).toNumber() : -1;
+	double max_inertia = context->argument(13).isNumber() ? context->argument(13).toNumber() : -1;
+	bool filter_by_inertia = min_inertia > 0 && max_inertia > 0 && context->argument(11).isBool() && context->argument(11).toBool();
+	
+	return engine->toScriptValue(new BlobTpl(
+		min_hsv, max_hsv,
+		filter_by_area, min_area, max_area,
+		filter_by_circularity, min_circularity, max_circularity,
+		filter_by_convexity, min_convexity, max_convexity,
+		filter_by_inertia, min_inertia, max_inertia
+	));
 }
--- botfather/src/vision/blob_tpl_factory.h
+++ botfather/src/vision/blob_tpl_factory.h
@@ -3,7 +3,7 @@
 
 #include <QObject>
 #include <QString>
-#include <QtScript/QScriptEngine>
+#include <QScriptEngine>
 #include "hsv_color.h"
 
 class BlobTplFactory : public QObject
@@ -12,25 +12,8 @@ class BlobTplFactory : public QObject
 	
 public:
 	BlobTplFactory(QScriptEngine* engine_p);
-	
-	Q_INVOKABLE QScriptValue createInstance(
-		HSVColor* min_hsv,
-		HSVColor* max_hsv,
-		bool filter_by_area,
-		int min_area = 0.0,
-		int max_area = 0.0,
-		bool filter_by_circularity = false,
-		double min_circularity = 0.0,
-		double max_circularity = 1.0,
-		bool filter_by_convexity = false,
-		double min_convexity = 0.0,
-		double max_convexity = 1.0,
-		bool filter_by_inertia = false,
-		double min_inertia_ration = 0.0,
-		double max_inertia_ration = 1.0
-	);
-	
 	static void enable(QScriptEngine* engine_p);
+	static QScriptValue create(QScriptContext *context, QScriptEngine *engine);
 
 private:
 	QScriptEngine* m_engine_p;
