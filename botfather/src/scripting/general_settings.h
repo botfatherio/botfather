46ec4ddfcacc50cbad46427b9da58da0ed3dc5d6
DoctorJohn <github@ehwald.info>
2018-05-26 04:50:27 +0200

    Fix general section name being reserved in the .ini format

--- botfather/src/scripting/general_settings.h
+++ botfather/src/scripting/general_settings.h
@@ -2,11 +2,13 @@
 #define BFP__SCRIPTING__GENERAL_SETTINGS_H
 #include <QString>
 
+// NOTE: One can not name a .ini file group "general", it'S a reserved name.
+
 namespace general
 {
 	namespace options {
-		static const QString AUTOKILL = "general/autokill";
-		static const QString AUTOKILL_PERIOD = "general/autokill_period";
+		static const QString AUTOKILL = "scripts/autokill";
+		static const QString AUTOKILL_PERIOD = "scripts/autokill_period";
 		static const QString STOP_SHORTCUT = "shortcuts/stop_shortcut";
 		static const QString KILL_SHORTCUT = "shortcuts/kill_shortcut";
 	}
