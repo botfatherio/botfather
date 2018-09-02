705faf0c0fd137b1308de94b59b4d422f92b661a
DoctorJohn <github@ehwald.info>
2018-09-02 15:20:37 +0200

    Remove unused code from blob_tpl files

--- botfather/src/engine/prototypes/blob_tpl_prototype.h
+++ botfather/src/engine/prototypes/blob_tpl_prototype.h
@@ -51,44 +51,7 @@ public:
 	// 1 <= new_threshold_step <= max threshold - min threshold
 	void setThresholdStep(int new_threshold_step);
 	int getThresholdStep() const;
-	
-	
-	
-	
-	
-	
-	
-	
-	/*
-	// Sets the blobs area to new_area
-	Q_INVOKABLE void setArea(int new_area);
-	
-	// Returns the blobs area
-	Q_INVOKABLE int getArea() const;
-	
-	// Sets the blobs circularity to new_circularity. 0 <= new_circularity <= 1.0f
-	Q_INVOKABLE void setCircularity(float new_circularity);
-	
-	// Returns the blobs circularity
-	Q_INVOKABLE float getCircularity() const;
-		
-	// Sets the blobs convexity to new_convexity. 0 <= new_convexity <= 1.0f
-	Q_INVOKABLE void setConvexity(float new_convexity);
-	
-	// Returns the blobs convexity
-	Q_INVOKABLE float getConvexity() const;
-	
-	// Sets the blobs inertia ratio to new_inertia. 0 <= new_inertia_ratio <= 1.0f
-	Q_INVOKABLE void setInertiaRatio(float new_inertia_ratio);
-	
-	// Returns the blobs inertia ratio
-	Q_INVOKABLE float getInertiaRatio() const;
-	
-	// 0 <= new_threshold <= 255
-	Q_INVOKABLE void setThreshold(int new_threshold);
-	Q_INVOKABLE int getThreshold() const;
-	*/
-	
+
 	Q_INVOKABLE QString toString() const;
 };
 
