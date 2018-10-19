122d0eef1d8108134ae66c2f65b770d387e8d601
Jonathan Ehwald <github@ehwald.info>
2018-10-19 21:17:00 +0200

    No longer show the status dialog

--- botfather/main.cpp
+++ botfather/main.cpp
@@ -6,7 +6,6 @@
 #include <QFileInfo>
 #include <QMessageBox>
 #include "gui/control_window.h"
-#include "gui/status_dialog.h"
 #include "engine/modules/browser/browser.h"
 
 #if defined(_WIN32) || defined(_WIN64)
@@ -124,37 +123,21 @@ int main(int argc, char *argv[])
 		}
 	}
 #endif
-	
-	bool browser_initialized = false;
-	StatusDialog *status_dialog = new StatusDialog(VERSION_SECRET);
-	
-	// Initialize the browser and start the controlwindow only when this version is supported
-	QObject::connect(status_dialog, &StatusDialog::accepted, [&](){
+
 	#if defined(_WIN32) || defined(_WIN64)
 		Browser::init(__argc, __argv);
 	#else
 		Browser::init(argc, argv);
 	#endif
-		browser_initialized = true;
-		
-		ControlWindow *control_window = new ControlWindow;
-		control_window->show();
-	});
 
-	// Quit the application if it's version is not supported. Exec returns 1 if the dialog has been accepted and 0 if rejected.
-	// Rejected means our rest api told us this version is not supported.
-	if (status_dialog->exec() == 0) {
-		return 0;
-	}
+	ControlWindow *control_window = new ControlWindow;
+	control_window->show();
 
 	// Runs the QApplication event loop blocking. When the event loop stops the timer
 	// powering the CEF event loop will stop aswell. After that the CEF can be shut
 	// down calling Browser::quit(). Subsequently both cef and qt event and message loops
 	// stopped, the applications exit code can be returned.
 	int return_code = a.exec();
-	if (browser_initialized) {
-		// Trying to quit the browser when it has not been initialized causes the client to crash
-		Browser::quit();
-	}
+	Browser::quit();
 	return return_code;
 }
