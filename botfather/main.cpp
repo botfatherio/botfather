b9b5239394722ad0168f15a0a951b86ce5543ba2
DoctorJohn <github@ehwald.info>
2018-10-30 13:47:30 +0100

    Bump version number to 4.3.4

--- botfather/main.cpp
+++ botfather/main.cpp
@@ -66,7 +66,7 @@ int main(int argc, char *argv[])
 	QCoreApplication::setOrganizationName("botfather.io");
 	QCoreApplication::setOrganizationDomain("botfather.io");
 	QCoreApplication::setApplicationName("Botfather");
-	QCoreApplication::setApplicationVersion("4.3.3");
+	QCoreApplication::setApplicationVersion("4.3.4");
 	QSettings::setDefaultFormat(QSettings::IniFormat);
 
 #if QT_VERSION < QT_VERSION_CHECK(5, 10, 0)
