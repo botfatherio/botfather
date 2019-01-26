ad7a321b782329e87a75bac38606fafdadead499
DoctorJohn <github@ehwald.info>
2019-01-26 04:47:51 +0100

    Add methods and signals to the Bot model to make it runnable

--- botfather/gui/models/bot.cpp
+++ botfather/gui/models/bot.cpp
@@ -6,11 +6,9 @@ Bot::Bot(QObject *parent) : QObject(parent)
 
 }
 
-Bot::Bot(const Bot::Data &data, QObject *parent)
-	: QObject(parent)
-	, m_data(data)
+Bot::Bot(const Bot::Data &data, QObject *parent) : Bot(parent)
 {
-
+	m_data = data;
 }
 
 bool Bot::isNull() const
@@ -23,14 +21,14 @@ bool Bot::isValid() const
 	return !isNull() && QDir(m_data.path).exists();
 }
 
+bool Bot::isRunning() const
+{
+	return m_is_running;
+}
+
 Bot::Data Bot::data() const
 {
 	return m_data;
-}
-
-void Bot::setData(const Bot::Data &data)
-{
-	m_data = data;
 }
 
 QString Bot::path() const
@@ -51,6 +49,7 @@ QString Bot::name() const
 void Bot::setName(const QString &name)
 {
 	m_data.name = name;
+	emit nameChanged(name);
 }
 
 QString Bot::repo() const
@@ -63,3 +62,62 @@ void Bot::setRepo(const QString &repo)
 	m_data.repo = repo;
 }
 
+QString Bot::scriptPath() const
+{
+	QDir repo_dir(path());
+	if (!isValid() || repo_dir.isEmpty())
+	{
+		return QString(); // Invalid or empty bot directory
+	}
+
+	QStringList scriptname_filters;
+	scriptname_filters << "*.js";
+
+	// Don't check for readability yet, the bot engine informs the user about permission issues
+	QStringList entries = repo_dir.entryList(scriptname_filters, QDir::Files|QDir::NoDotAndDotDot);
+
+	if (entries.isEmpty())
+	{
+		return QString(); // No script found
+	}
+
+	// TODO: extend to check in the scripts manifest for the main scriptfiles path.
+
+	return repo_dir.filePath(entries.first());
+}
+
+void Bot::start()
+{
+	if (isRunning()) return;
+
+	m_engine_thread = new QThread;
+	m_engine_thread->setObjectName("Engine thread for " + name());
+
+	m_engine = new Engine(scriptPath());
+	m_engine->moveToThread(m_engine_thread);
+
+	connect(m_engine_thread, &QThread::started, m_engine, &Engine::runScript);
+	connect(m_engine, &Engine::stopped, m_engine_thread, &QThread::quit);
+	connect(m_engine, &Engine::stopped, m_engine, &QObject::deleteLater);
+	connect(m_engine_thread, &QThread::finished, m_engine_thread, &QObject::deleteLater);
+
+	connect(m_engine_thread, &QThread::finished, [this](){
+		m_is_running = false;
+		emit stopped();
+	});
+
+	connect(m_engine, &Engine::log, this, &Bot::log);
+	connect(m_engine, &Engine::playWavSound, this, &Bot::audioPlaybackRequested);
+	connect(m_engine, &Engine::stopWavSound, this, &Bot::audioStopRequested);
+
+	m_engine_thread->start();
+	m_is_running = true;
+	emit started();
+}
+
+void Bot::stop()
+{
+	if (!isRunning()) return;
+	Q_ASSERT(m_engine);
+	m_engine->stop();
+}
--- botfather/gui/models/bot.h
+++ botfather/gui/models/bot.h
@@ -35,8 +35,9 @@ public:
 	// Returns true if the bot isNull returns false and the bots path exists.
 	bool isValid() const;
 
+	bool isRunning() const;
+
 	Bot::Data data() const;
-	void setData(const Bot::Data &data);
 
 	// The path to the folder in which the bots files are stored. Use a bots path to identify it.
 	QString path() const;
@@ -50,15 +51,27 @@ public:
 	QString repo() const;
 	void setRepo(const QString &repo);
 
+	// Returns the path to the first script file found in the bots directory or an empty string if nothing has been found.
+	QString scriptPath() const;
+
 signals:
+	void nameChanged(const QString &new_name);
+	void log(const QString &message, const Engine::LogSource &source);
+	void audioPlaybackRequested(const QString &path_to_wav_file);
+	void audioStopRequested();
+
+	void started();
+	void stopped();
 
 public slots:
+	void start();
+	void stop();
 
 private:
+	bool m_is_running = false;
 	Bot::Data m_data;
-
-	//Engine *m_engine;
-	//QThread *m_engine_thread;
+	Engine *m_engine;
+	QThread *m_engine_thread;
 };
 
 Q_DECLARE_METATYPE(Bot*)
