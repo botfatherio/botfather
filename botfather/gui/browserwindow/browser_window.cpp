556356e7998d2da802d98839481cbf20cca640fa
DoctorJohn <github@ehwald.info>
2019-06-08 04:36:15 +0200

    Make browser viewable using the new browser window

--- botfather/gui/browserwindow/browser_window.cpp
+++ botfather/gui/browserwindow/browser_window.cpp
@@ -11,51 +11,16 @@
 
 BrowserWindow::BrowserWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::BrowserWindow)
 {
-    ui->setupUi(this);
-
-	// Setup a singleshot timer used to resize the browser once the user stopped
-	// resizing the whole window.
-	resize_timer.setSingleShot(true);
-	connect(&resize_timer, SIGNAL(timeout()), SLOT(applyResizing()));
-	
-	// Setup a min and max size for the window and the pixmap placeholder
-	this->setMinimumSize(512, 256);
-	this->setMaximumSize(4096, 2160);
-	
-	// Add a modified label widget to this window which will take care of
-	// mouse and keyboard events.
+	ui->setupUi(this);
 	pixmap_placeholder = new BrowserWidget(this);
-	this->ui->centralwidget->layout()->addWidget(pixmap_placeholder);
-
-	// Add a browser address bar to the browser window.
 	addressbar = new BrowserAddressBar(this);
+	ui->centralwidget->layout()->addWidget(pixmap_placeholder);
 	ui->toolBar->addWidget(addressbar);
-
-	// Remember and set the browser dialogs geometry.
-	QSettings settings;
-	int width = settings.value(browser::options::WIDTH, browser::fallback::WIDTH).toInt();
-	int height = settings.value(browser::options::HEIGHT, browser::fallback::HEIGHT).toInt();
-	int toolbar_height = this->ui->toolBar->height();
-	this->setGeometry(0, 0, width, height + toolbar_height);
-	
-	// Connect browser window action singals with their respectiv offscreen browser slots.
-	/*
-	connect(ui->actionReload, &QAction::triggered, OldBrowser::reload);
-	connect(ui->actionStop, &QAction::triggered, OldBrowser::stopLoad);
-	connect(ui->actionBack, &QAction::triggered, OldBrowser::goBack);
-	connect(ui->actionForward, &QAction::triggered, OldBrowser::goForward);
-	
-	// Update our navigation buttons every time the browser clients loading state changes.
-	connect(OldBrowserClient::instance(), SIGNAL(loadingStateChangedSignal(bool)), this, SLOT(updateNavigationButtons(bool)));
-	*/
 }
 
 BrowserWindow::~BrowserWindow()
 {
 	delete ui;
-	delete pixmap_placeholder;
-	delete address_bar;
-	delete addressbar;
 }
 
 void BrowserWindow::show()
@@ -70,111 +35,7 @@ void BrowserWindow::show()
     QMainWindow::show();
 }
 
-void BrowserWindow::applyResizing()
-{
-	/*
-	// The user stopped resizing the window. Resize the browser accordingly.
-	int width = this->pixmap_placeholder->width();
-	int height = this->pixmap_placeholder->height();
-	OldBrowser::resize(QSize(width, height));
-	
-	// Redirect the first few paint event signals to a slot filtering all browser
-	// images which are still of the windows old size to prevent the window from
-	// going back to it's old size due to the pixmap being potentially as large as
-	// the old window size was. Which may happen due to the browsers resize method
-	// being async.
-	connect(OldBrowserClient::instance(), SIGNAL(paintSignal(QImage)), this, SLOT(filterOldSize(QImage)));
-	*/
-}
-
-void BrowserWindow::filterOldSize(QImage browser_image)
-{
-	/*
-	if (browser_image.size() == this->pixmap_placeholder->size()) {
-		
-		// Render this first correct image to the window to make the "Resizing
-		// Offscreen Browser" message disappear to not confuse the user.
-		paintSlot(browser_image);
-		
-		// The browser now sends images of the placeholders size. Disconnect the filter
-		// redirect the paint signals to the normal paint slot.
-		disconnect(OldBrowserClient::instance(), SIGNAL(paintSignal(QImage)), this, SLOT(filterOldSize(QImage)));
-		connect(OldBrowserClient::instance(), SIGNAL(paintSignal(QImage)), this, SLOT(paintSlot(QImage)));
-	}
-	*/
-}
-
 void BrowserWindow::paintSlot(QImage browser_image)
 {
-	this->pixmap_placeholder->setPixmap(QPixmap::fromImage(browser_image));
-}
-
-void BrowserWindow::on_actionHome_triggered()
-{
-	//OldBrowser::loadUrl("about:version");
-}
-
-void BrowserWindow::updateNavigationButtons(bool browser_loading_state)
-{
-	/*
-	this->ui->actionBack->setEnabled(OldBrowser::canGoBack());
-	this->ui->actionForward->setEnabled(OldBrowser::canGoForward());
-	if (browser_loading_state) {
-		ui->actionReload->setVisible(false);
-		ui->actionStop->setVisible(true);
-	} else {
-		ui->actionReload->setVisible(true);
-		ui->actionStop->setVisible(false);
-	}
-	*/
-}
-
-void BrowserWindow::showEvent(QShowEvent *event)
-{
-	Q_UNUSED(event)
-	
-	// Attract mouse and keyboard events.
-	this->setMouseTracking(true);
-	this->setFocusPolicy(Qt::ClickFocus);
-	
-	// Start rendering the website content to the window when it's shown.
-	//QObject::connect(OldBrowserClient::instance(), SIGNAL(paintSignal(QImage)), this, SLOT(paintSlot(QImage)));
-}
-
-void BrowserWindow::hideEvent(QHideEvent *event)
-{
-	Q_UNUSED(event)
-	
-	// Stop rendering the websites content when the browser dialog gets closed.
-	//QObject::disconnect(OldBrowserClient::instance(), SIGNAL(paintSignal(QImage)), this, SLOT(paintSlot(QImage)));
-}
-
-void BrowserWindow::resizeEvent(QResizeEvent *event)
-{
-	Q_UNUSED(event)
-	
-	// No longer paint stuff before the user decided what size he wants.
-	// We do no longer paint because qt tends to do segmentation faults.
-	// Thats due to qt and cef being async and having seperate event loops.
-	
-	// Save ressources by only clearing the pixmap and settings the info text once.
-	/*
-	if (disconnect(OldBrowserClient::instance(), SIGNAL(paintSignal(QImage)), this, SLOT(paintSlot(QImage)))){
-		
-		// Remove the last painted pixmap to enable the user to freely resize the
-		// browser dialog. Not clearing the pixmap makes it impossible to shrink
-		// the dialog, because the pixmap being as large as the window is.
-		this->pixmap_placeholder->clear();
-		
-		// Show a 'Resizing...' message while resizing. We do so because the user
-		// might otherwise wonder why the pixmap is no longer displayed.
-		this->pixmap_placeholder->setText(
-			"<p style='font-size:32px;font-weight:700;color:#333;'>Resizing Offscreen Browser...</p>"
-		);
-	}
-	*/
-	
-	// This timer will timeout when the user finally stops the resize the window.
-	// We do this to not have to process every single resize event on every pixel change.
-	resize_timer.start(500);
+	pixmap_placeholder->setPixmap(QPixmap::fromImage(browser_image));
 }
--- botfather/gui/browserwindow/browser_window.h
+++ botfather/gui/browserwindow/browser_window.h
@@ -2,9 +2,7 @@
 #define BFP__GUI__BROWSER_WINDOW_H
 
 #include <QMainWindow>
-#include <QLineEdit>
 #include <QImage>
-#include <QTimer>
 
 class BrowserWidget;
 class BrowserAddressBar;
@@ -22,22 +20,17 @@ public:
 	~BrowserWindow();
 	
 public slots:
-    void show();
-	void applyResizing();
-	void filterOldSize(QImage browser_image);
+	void show();
 	void paintSlot(QImage browser_image);
-	void on_actionHome_triggered();
-	void updateNavigationButtons(bool browser_loading_state);
 	
+signals:
+	//void sendClick();
+	//void sendUrl();
+
 private:
 	Ui::BrowserWindow *ui;
 	BrowserWidget* pixmap_placeholder;
-	BrowserAddressBar* address_bar;
-	QLineEdit* addressbar;
-	QTimer resize_timer;
-	void showEvent(QShowEvent * event);
-	void hideEvent(QHideEvent *event);
-	void resizeEvent(QResizeEvent *event);
+	BrowserAddressBar* addressbar;
 };
 
 #endif // BFP__GUI__BROWSER_WINDOW_H
