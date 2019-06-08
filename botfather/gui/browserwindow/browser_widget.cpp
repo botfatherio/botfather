676b273ece8ce69e9d3e54418a83cf9a463e2f58
DoctorJohn <github@ehwald.info>
2019-06-08 21:43:22 +0200

    Make the BrowserWidget emit signals when input events occur

--- botfather/gui/browserwindow/browser_widget.cpp
+++ botfather/gui/browserwindow/browser_widget.cpp
@@ -1,83 +1,46 @@
 #include "browser_widget.h"
 #include <QMouseEvent>
-#include <QMessageBox>
+#include <QDebug>
 
 BrowserWidget::BrowserWidget(QWidget* parent) : QLabel(parent)
 {
 	// Make this widget expand to it's parents size.
-	this->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+	setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
 	
 	// Make this widgets content (like the resizing text) be centered.
-	this->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+	setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
 	
 	// Attract mouse and keyboard events.
-	this->setMouseTracking(true);
-	this->setFocusPolicy(Qt::ClickFocus);
+	setMouseTracking(true);
+	setFocusPolicy(Qt::ClickFocus);
 }
 
 void BrowserWidget::mousePressEvent(QMouseEvent *event)
 {
-	/*
-	int cef_button_code = OldBrowser::qtToCefMouseButtonType(static_cast<int>(event->button()));
-	if (cef_button_code == -1) {
-		return;
-	}
-	OldBrowser::pressMouse(cef_button_code, event->pos());
-	*/
+	emit mousePressed(event->pos(), event->button());
 }
 
 void BrowserWidget::mouseReleaseEvent(QMouseEvent *event)
 {
-	/*
-	int cef_button_code = OldBrowser::qtToCefMouseButtonType(static_cast<int>(event->button()));
-	if (cef_button_code == -1) {
-		return;
-	}
-	OldBrowser::releaseMouse(cef_button_code, event->pos());
-	*/
+	emit mouseReleased(event->pos(), event->button());
 }
 
 void BrowserWidget::mouseMoveEvent(QMouseEvent *event)
 {
-	//OldBrowser::moveMouse(event->pos());
+	emit mouseMoved(event->pos());
 }
 
 void BrowserWidget::wheelEvent(QWheelEvent *event)
 {
-	//OldBrowser::scrollWheel(event->pos(), event->angleDelta().x(), event->angleDelta().y());
+	emit wheelScrolled(event->pos(), event->angleDelta());
 }
 
 void BrowserWidget::keyPressEvent(QKeyEvent *event)
 {
-	/*
-	if (event->matches(QKeySequence::Copy))
-	{
-		OldBrowserClient::instance()->getBrowser()->GetFocusedFrame()->Copy();
-	}
-	else if (event->matches(QKeySequence::Cut))
-	{
-		OldBrowserClient::instance()->getBrowser()->GetFocusedFrame()->Cut();
-	}
-	else if (event->matches(QKeySequence::Paste))
-	{
-		OldBrowserClient::instance()->getBrowser()->GetFocusedFrame()->Paste();
-	}
-	else if (event->matches(QKeySequence::Undo))
-	{
-		OldBrowserClient::instance()->getBrowser()->GetFocusedFrame()->Undo();
-	}
-	else if (event->matches(QKeySequence::Redo))
-	{
-		OldBrowserClient::instance()->getBrowser()->GetFocusedFrame()->Redo();
-	}
-	else if (event->matches(QKeySequence::SelectAll))
-	{
-		OldBrowserClient::instance()->getBrowser()->GetFocusedFrame()->SelectAll();
-	}
-	*/
+	emit keyPressed(*event);
 }
 
 void BrowserWidget::keyReleaseEvent(QKeyEvent *event)
 {
-	Q_UNUSED(event)
+	emit keyReleased(*event);
 }
--- botfather/gui/browserwindow/browser_widget.h
+++ botfather/gui/browserwindow/browser_widget.h
@@ -2,7 +2,6 @@
 #define BFP__GUI__BROWSER_WIDGET_H
 
 #include <QLabel>
-#include <QImage>
 
 class BrowserWidget : public QLabel {
 	Q_OBJECT
@@ -10,13 +9,21 @@ class BrowserWidget : public QLabel {
 public:
 	explicit BrowserWidget(QWidget* parent = nullptr);
 
+signals:
+	void mousePressed(const QPoint &position, int qt_mouse_button);
+	void mouseReleased(const QPoint &position, int qt_mouse_button);
+	void mouseMoved(const QPoint &position);
+	void wheelScrolled(const QPoint &position, const QPoint &delta);
+	void keyPressed(const QKeyEvent &event);
+	void keyReleased(const QKeyEvent &event);
+
 private:
 	void mousePressEvent(QMouseEvent *event);
 	void mouseReleaseEvent(QMouseEvent *event);
 	void mouseMoveEvent(QMouseEvent *event);
-	void wheelEvent(QWheelEvent* event);
-	void keyPressEvent(QKeyEvent * event);
-	void keyReleaseEvent(QKeyEvent * event);
+	void wheelEvent(QWheelEvent *event);
+	void keyPressEvent(QKeyEvent *event);
+	void keyReleaseEvent(QKeyEvent *event);
 };
 
 #endif // BFP__GUI__BROWSER_WIDGET_H
