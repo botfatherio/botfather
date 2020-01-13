df785d76f46012c422df2620ef75af1221f2fda6
DoctorJohn <github@ehwald.info>
2020-01-13 23:44:43 +0100

    Upgrade code to work with OpenCV 4.2.0

--- botfather/engine/modules/vision/vision.cpp
+++ botfather/engine/modules/vision/vision.cpp
@@ -11,7 +11,7 @@ cv::Mat Vision::isolateColor(const cv::Mat &image, const cv::Scalar &min_hsv, co
 	cv::UMat threshold_umat, inverted_threshold;
 
 	// Convert from BGR to HSV, because cv::inRange only works with HSV mats.
-	cv::cvtColor(image, hsv_umat, CV_BGR2HSV);
+	cv::cvtColor(image, hsv_umat, cv::COLOR_BGR2HSV);
 
 	// inRange produces a single channel binary matrix, i.e. a CV_8UC1 matrix
 	// with values either 0 or 255. Note: Gimp2 shows you hsv in %.
@@ -29,7 +29,7 @@ cv::Mat Vision::isolateColor(const cv::Mat &image, const cv::Scalar &min_hsv, co
 	}
 
 	// Convert HSV back to BGR, because we use BGR in our application.
-	cv::cvtColor(hsv_umat, bgr_result_umat, CV_HSV2BGR);
+	cv::cvtColor(hsv_umat, bgr_result_umat, cv::COLOR_HSV2BGR);
 
 	hsv_umat.release();
 	threshold_umat.release();
@@ -41,14 +41,14 @@ cv::Mat Vision::isolateColor(const cv::Mat &image, const cv::Scalar &min_hsv, co
 int Vision::countDifferentPixels(const cv::Mat &image_1, const cv::Mat &image_2)
 {
 	cv::Mat image_1_gray, image_2_gray, diff;
-	
+
 	// Make both images grayscale (for better performance)
-	cv::cvtColor(image_1, image_1_gray, CV_BGR2GRAY);
-	cv::cvtColor(image_2, image_2_gray, CV_BGR2GRAY);
-	
+	cv::cvtColor(image_1, image_1_gray, cv::COLOR_BGRA2BGR);
+	cv::cvtColor(image_2, image_2_gray, cv::COLOR_BGR2GRAY);
+
 	// CMP_NE: image_1 is unequal to image_2.
 	cv::compare(image_1_gray, image_2_gray, diff, cv::CMP_NE);
-	
+
 	image_1_gray.release();
 	image_2_gray.release();
 
@@ -59,47 +59,47 @@ int Vision::countDifferentPixels(const cv::Mat &image_1, const cv::Mat &image_2)
 double Vision::histogramSimilarity(const cv::Mat &image_1, const cv::Mat &image_2)
 {
 	cv::Mat hsv_1, hsv_2;
-	cv::cvtColor(image_1, hsv_1, CV_BGR2HSV);
-	cv::cvtColor(image_2, hsv_2, CV_BGR2HSV);
-	
+	cv::cvtColor(image_1, hsv_1, cv::COLOR_BGR2HSV);
+	cv::cvtColor(image_2, hsv_2, cv::COLOR_BGR2HSV);
+
 	// Using 50 bins for hue and 60 for saturation
 	int h_bins = 50;
 	int s_bins = 60;
 	int v_bins = 60;
 	int hist_size[] = { h_bins, s_bins, v_bins };
-	
+
 	// Use the o-th and 1-st channels
 	int channels[] = { 0, 1, 2 };
-	
+
 	// hue varies from 0 to 179, saturation from 0 to 255
 	float h_ranges[] = { 0, 180 };
 	float s_ranges[] = { 0, 256 };
 	float v_ranges[] = { 0, 256 };
 	const float* ranges[] = { h_ranges, s_ranges, v_ranges };
-	
+
 	int dims = 3;
-	
+
 	cv::MatND hsv_1_hist, hsv_2_hist;
-	
+
 	cv::calcHist(&hsv_1, 1, channels, cv::Mat(), hsv_1_hist, dims, hist_size, ranges, true, false);
 	cv::normalize(hsv_1_hist, hsv_1_hist, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
-	
+
 	cv::calcHist(&hsv_2, 1, channels, cv::Mat(), hsv_2_hist, dims, hist_size, ranges, true, false);
-	cv::normalize(hsv_2_hist, hsv_2_hist, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());	
-	
+	cv::normalize(hsv_2_hist, hsv_2_hist, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
+
 	hsv_1.release();
 	hsv_2.release();
 
-	return cv::compareHist(hsv_1_hist, hsv_2_hist, CV_COMP_CORREL);
+	return cv::compareHist(hsv_1_hist, hsv_2_hist, cv::HISTCMP_CORREL);
 }
 
 QVector<Match> Vision::findMaskedMatches(const cv::Mat &image, const cv::Mat &tpl, const cv::Mat &mask, double threshold, int max_matches)
 {
 	// Note: Only CV_TM_SQDIFF and CV_TM_CCORR_NORMED accept maskes.
-	static const int match_method = CV_TM_CCORR_NORMED;
-	bool match_method_accepts_mask = (CV_TM_SQDIFF == match_method || match_method == CV_TM_CCORR_NORMED);
+	static const int match_method = cv::TM_CCORR_NORMED;
+	bool match_method_accepts_mask = (cv::TM_SQDIFF == match_method || match_method == cv::TM_CCORR_NORMED);
 	bool use_mask = !mask.empty();
-	
+
 	QVector<Match> matches;
 
 	cv::UMat uimage = image.getUMat(cv::ACCESS_RW);
@@ -107,13 +107,13 @@ QVector<Match> Vision::findMaskedMatches(const cv::Mat &image, const cv::Mat &tp
 
 	double min_val, max_val, match_val;
 	cv::Point min_loc, max_loc, match_loc;
-	
+
 	// Create a result matrix of the images size. NOTE: Saving the result image doesn't work
-	// for some reason. But 
+	// for some reason. But
 	// We tested saving it using: cv::imwrite("result.png", result.getMat(cv::ACCESS_READ).clone());
 	// But result.png is always black, no matter where in this method we tried to save it.
 	cv::UMat uresult(uimage.rows - utpl.rows + 1, uimage.cols - utpl.cols + 1, CV_32FC1);
-	
+
 	// cv::matchTemplate requires the image to have a depth of 0 or 5, and both the image and the template
 	// to gave the same types. The images dims must be equal or less 2.
 	Q_ASSERT(uimage.depth() == 0 || uimage.depth() == 5);
@@ -127,36 +127,36 @@ QVector<Match> Vision::findMaskedMatches(const cv::Mat &image, const cv::Mat &tp
 	} else {
 		cv::matchTemplate(uimage, utpl, uresult, match_method);
 	}
-	
+
 	// Makes not so good results better which results in "better" matches in some
 	// cases but in wrong matches if there is actually nothing to match.
 	// Note: Using this method fucked up everything already 3 times!
 	//cv::normalize( result, result, 0, 1, cv::NORM_MINMAX, -1, cv::Mat() );
-	
+
 	// Make areas completely black which are not as intensive as the threshold
 	// requires. Looking for minmax values and using floodfill are faster after
 	// thresholding.
-	cv::threshold(uresult, uresult, threshold, 1.0, CV_THRESH_TOZERO);
-	
+	cv::threshold(uresult, uresult, threshold, 1.0, cv::THRESH_TOZERO);
+
 	// Uncomment the following 2 lines of code to see where matches were found.
 	// One may disabled thresholding to understand the result better.
 	//cv::namedWindow("Result", cv::WINDOW_AUTOSIZE);
 	//cv::imshow("Result", result);
-	
+
 	while (matches.size() < max_matches || max_matches == -1){
-		
+
 		// Find the lightest spot aka the best matching location
 		cv::minMaxLoc(uresult, &min_val, &max_val, &min_loc, &max_loc, cv::Mat());
-		
+
 		// For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
-		if (match_method == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED) {
+		if (match_method == cv::TM_SQDIFF || match_method == cv::TM_SQDIFF_NORMED) {
 			match_val = 1.0 - min_val;
 			match_loc = min_loc;
 		} else{
 			match_val = max_val;
 			match_loc = max_loc;
 		}
-		
+
 		// Stop looking for more matching when the matches threshold becomes to low.
 		if (match_val < threshold) {
 			break;
@@ -171,8 +171,8 @@ QVector<Match> Vision::findMaskedMatches(const cv::Mat &image, const cv::Mat &tp
 		// reliable.
 		// NOTE: Whatever method is used, one has to respect the user match method as
 		// different methods mark the best matching location either black OR white.
-		
-		if (match_method == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED) {
+
+		if (match_method == cv::TM_SQDIFF || match_method == cv::TM_SQDIFF_NORMED) {
 			// Here we use the color white to hide processed matches.
 			// A thickness of -1 means the circle shall be filled.
 			cv::circle(uresult, match_loc, utpl.cols / 2, cv::Scalar(255), -1);
@@ -180,13 +180,13 @@ QVector<Match> Vision::findMaskedMatches(const cv::Mat &image, const cv::Mat &tp
 			// ... black to hide processed matches.
 			cv::circle(uresult, match_loc, utpl.cols / 2, cv::Scalar(0), -1);
 		}
-		
+
 		// Format the match and push it to the other matches. x and y of match_loc are
 		// the left and top coordinates of the match. Note: in our Match class x and y
 		// mark the center of the match.
 		matches.push_back(Match(QRect(match_loc.x, match_loc.y, utpl.cols, utpl.rows), match_val));
 	}
-	
+
 	uimage.release();
 	utpl.release();
 	uresult.release();
@@ -222,19 +222,19 @@ QVector<Match> Vision::findBlobs(const cv::Mat &image, const cv::SimpleBlobDetec
 	cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(blob_params);
 	std::vector<cv::KeyPoint> key_points;
 	detector->detect(image_umat, key_points);
-	
+
 	QVector<Match> matches;
-	
+
 	for (cv::KeyPoint kp : key_points)
-	{	
+	{
 		int size = static_cast<int>(kp.size);
 		int left = static_cast<int>(kp.pt.x) - size / 2;
 		int top = static_cast<int>(kp.pt.y) - size / 2;
-		
+
 		Match match(QRect(left, top, size, size), 1);
 		matches.append(match);
 	}
-	
+
 	image_umat.release();
 	return matches;
 }
@@ -242,18 +242,18 @@ QVector<Match> Vision::findBlobs(const cv::Mat &image, const cv::SimpleBlobDetec
 QImage Vision::markMatches(const QImage &image, const QVector<Match> &matches, const QColor &color, int thickness)
 {
 	QImage result = image;
-	
+
 	QPen pen;
 	pen.setColor(color);
 	pen.setWidth(thickness);
-	
+
 	QPainter painter(&result);
 	painter.setPen(pen);
-	
+
 	for (Match match : matches) {
 		painter.drawRect(match);
 	}
-	
+
 	painter.end();
 	return result;
 }
@@ -261,7 +261,7 @@ QImage Vision::markMatches(const QImage &image, const QVector<Match> &matches, c
 cv::Mat Vision::qimageToBGRMat(const QImage &qimage)
 {
 	switch(qimage.format()) {
-	
+
 	// 8-bit, 4 channel
 	case QImage::Format_ARGB32:
 	case QImage::Format_ARGB32_Premultiplied: {
@@ -275,7 +275,7 @@ cv::Mat Vision::qimageToBGRMat(const QImage &qimage)
 
 		return mat.clone();
 	}
-	
+
 	// 8-bit, 3 channel
 	case QImage::Format_RGB32: {
 		cv::Mat mat(
@@ -285,12 +285,12 @@ cv::Mat Vision::qimageToBGRMat(const QImage &qimage)
 			const_cast<uchar*>(qimage.bits()),
 			static_cast<size_t>(qimage.bytesPerLine())
 		);
-	 
+
 		cv::Mat mat_without_alpha;
 		cv::cvtColor(mat, mat_without_alpha, cv::COLOR_BGRA2BGR);
 		return mat_without_alpha;
 	}
-	 
+
 	// 8-bit, 3 channel
 	case QImage::Format_RGB888: {
 		QImage swapped = qimage.rgbSwapped();
@@ -302,7 +302,7 @@ cv::Mat Vision::qimageToBGRMat(const QImage &qimage)
 			static_cast<size_t>(swapped.bytesPerLine())
 		).clone();
 	}
-	 
+
 	// 8-bit, 1 channel
 	case QImage::Format_Indexed8: {
 		cv::Mat mat(
@@ -314,7 +314,7 @@ cv::Mat Vision::qimageToBGRMat(const QImage &qimage)
 		);
 		return mat.clone();
 	}
-	
+
 	default:
 		qWarning() << "QImage format not handled in switch:" << qimage.format();
 		break;
@@ -325,7 +325,7 @@ cv::Mat Vision::qimageToBGRMat(const QImage &qimage)
 QImage Vision::cvMatToQImage(const cv::Mat &mat)
 {
 	switch (mat.type()) {
-	
+
 	// 8-bit, 4 channel
     case CV_8UC4: {
 		QImage image(
@@ -337,7 +337,7 @@ QImage Vision::cvMatToQImage(const cv::Mat &mat)
 		);
 		return image;
 	}
- 
+
     // 8-bit, 3 channel
 	case CV_8UC3: {
 		QImage image(
@@ -349,7 +349,7 @@ QImage Vision::cvMatToQImage(const cv::Mat &mat)
 		);
 		return image.rgbSwapped();
 	}
- 
+
 	// 8-bit, 1 channel
 	case CV_8UC1: {
 		QImage image(
@@ -361,12 +361,12 @@ QImage Vision::cvMatToQImage(const cv::Mat &mat)
 		);
 		return image;
 	}
- 
+
     default:
 		qWarning() << "cv::Mat image type not handled in switch:" << mat.type();
 		break;
 	}
- 
+
 	return QImage();
 }
 
