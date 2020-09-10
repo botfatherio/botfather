e1af92b1752c9f2d574cfe912e707427b8cbfb85
Jonathan Ehwald <github@ehwald.info>
2020-09-11 00:53:51 +0200

    Add a color finder preview window

---
+++ botfather/gui/color_finder_preview/color_finder_preview.cpp
@@ -0,0 +1,27 @@
+#include "color_finder_preview.hpp"
+#include <QVBoxLayout>
+#include <QIcon>
+
+ColorFinderPreview::ColorFinderPreview(const QString &file_name, QWidget *parent)
+    : QWidget(parent)
+{
+    setWindowTitle("Color finder preview");
+    setWindowIcon(QIcon("://logo_v2.png"));
+
+    setLayout(new QVBoxLayout());
+    label = new QLabel(this);
+    layout()->addWidget(label);
+    layout()->setMargin(0);
+
+    original =	cv::imread(file_name.toStdString());
+    setFixedSize(QSize(original.cols, original.rows));
+}
+
+void ColorFinderPreview::updateColors(int min_h, int min_s, int min_v, int max_h, int max_s, int max_v)
+{
+    cv::Scalar min_hsv(min_h, min_s, min_v);
+    cv::Scalar max_hsv(max_h, max_s, max_v);
+    cv::Mat isolated = Vision::isolateColor(original, min_hsv, max_hsv, true);
+    QImage image = Vision::cvMatToQImage(isolated);
+    label->setPixmap(QPixmap::fromImage(image));
+}
---
+++ botfather/gui/color_finder_preview/color_finder_preview.hpp
@@ -0,0 +1,22 @@
+#ifndef COLORFINDERPREVIEW_HPP
+#define COLORFINDERPREVIEW_HPP
+
+#include <QWidget>
+#include <QLabel>
+#include "../../engine/modules/vision/vision.hpp"
+
+class ColorFinderPreview : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit ColorFinderPreview(const QString &file_name, QWidget *parent = nullptr);
+
+public slots:
+    void updateColors(int min_h, int min_s, int min_v, int max_h, int max_s, int max_v);
+
+private:
+    cv::Mat original;
+    QLabel *label;
+};
+
+#endif // COLORFINDERPREVIEW_HPP
